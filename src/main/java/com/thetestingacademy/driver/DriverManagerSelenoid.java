492fbb8faed425d535a76b01690666571e96b7e1
PANKAJG-TECH\Pankaj Gokhale <pankaj.gokhale@rebelfoods.com>
2025-01-17 13:59:52 +0530

    Advance

---
+++ src/main/java/com/thetestingacademy/driver/DriverManagerSelenoid.java
@@ -0,0 +1,6 @@
+package com.thetestingacademy.driver;
+
+public class DriverManagerSelenoid {
+    //Upgraded version to support the Multiple Parallel Testing
+    //In the cloud Grids-browserstack,lambdatest, docor container
+}
---
+++ src/main/java/com/thetestingacademy/driver/DriverManagerTL.java
@@ -0,0 +1,5 @@
+package com.thetestingacademy.driver;
+
+public class DriverManagerTL {
+    //Thread Local Support - Run Your TestCase Parallel
+}
